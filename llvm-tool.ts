f2c658d2eefbd60f2dcd60997e8cac5284422e15
twelsby <trevor@welsby.de>
2022-10-27 18:24:35 +0200

    Update llvm-tool.ts

--- llvm-tool.ts
+++ llvm-tool.ts
@@ -10,39 +10,19 @@ export class LLVMTool extends BaseTool {
         return 'llvm-tool';
     }
 
-    constructor(toolInfo, env) {
-        super(toolInfo, env);
-
-        this.addOptionsToToolArgs = false;
-    }
-
     async runTool(
         compilationInfo: Record<any, any>,
         inputFilepath?: string,
         args?: string[],
         stdin?: string,
-        supportedLibraries?: Record<string, Library>,
     ) {
-        if (this.tool.name) {
-            toolCounter.inc({
-                language: compilationInfo.compiler.lang,
-                name: this.tool.name,
-            });
-        }
         const execOptions = this.getDefaultExecOptions();
-        if (inputFilepath) execOpt
-        ions.customCwd = path.dirname(inputFilepath);
         execOptions.input = stdin;
 
         args = ['-S', '-emit-llvm', '-x', 'c++', '-o /dev/stdout', '|', this.tool.exe, '-o /dev/stdout'].concat(args);
-        if (this.addOptionsToToolArgs) args = this.tool.options.concat(args);
-        if (inputFilepath) args.push(inputFilepath);
-
-        const exeDir = path.dirname(this.tool.exe);
 
         try {
-            const result = await this.exec('/usr/bin/clang-11', args, execOptions);
-            return this.convertResult(result, inputFilepath, exeDir);
+            return await this.exec('/usr/bin/clang-11', args, execOptions);
         } catch (e) {
             logger.error('Error while running tool: ', e);
             return this.createErrorResponse('Error while running tool');

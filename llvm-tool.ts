a111b0352602d7266cd240af9a7306c30bed540a
twelsby <trevor@welsby.de>
2022-10-28 09:50:04 +0200

    Update llvm-tool.ts

--- llvm-tool.ts
+++ llvm-tool.ts
@@ -17,10 +17,7 @@ export class LLVMTool extends BaseTool {
         stdin: string,
     ) {
         const execOptions = this.getDefaultExecOptions();
-        execOptions.input = stdin;
-
-        const clangargs = ['-S', '-emit-llvm', '-x', 'c++'];
-        args = ['-o', '/dev/stdout', '-'].concat(args);
+        const clangargs = ['-S', '-emit-llvm', '-o-', inputFilepath];
 
         try {
             const cresult = await this.exec('/usr/bin/clang-11', clangargs, execOptions);
